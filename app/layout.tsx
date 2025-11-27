2e0040d4cbe6458e8efcf2f0d4744432ce772ea3
JhonAQ <jariasq@unsa.edu.pe>
2025-11-26 20:34:17 -0500

    feat: Cursor bolita por FIIINNNNNNN

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat, Poppins } from "next/font/google";
 import "./globals.css";
+import CustomCursor from "@/components/ui/CustomCursor";
 
 const montserrat = Montserrat({
   variable: "--font-title",
@@ -36,6 +37,7 @@ export default function RootLayout({
         />
       </head>
       <body className={`${montserrat.variable} ${poppins.variable}`}>
+        <CustomCursor />
         {children}
       </body>
     </html>
